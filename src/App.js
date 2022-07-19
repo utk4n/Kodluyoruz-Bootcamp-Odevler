9011af4d00b564412fe6e644812c9ab0049029d5
Utkan <41093352+utk4n@users.noreply.github.com>
2022-07-19 23:16:00 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import style from './components/app.module.css'
+import Navbar from './components/NavBar/Navbar';
+export default function App() {
+
+  return (
+    <div className={style.app}>
+      <Navbar />
+    </div>
+  );
+}
+
+
